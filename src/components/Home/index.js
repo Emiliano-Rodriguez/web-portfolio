915a5a79ff6cd69ef3b51ff313eab9b6c3012797
Emiliano Rodriguez <31290912+Emiliano-Rodriguez@users.noreply.github.com>
2025-01-19 12:12:11 -0600

    mobile support

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -14,23 +14,24 @@ const Home = () => {
           setLetterClass('text-animate-hover')
         }, 5000)
       }, [])
-    
-    
 
-    return ( 
+
+
+    return (
         <div className="container home-page">
             <div className="text-zone">
                 <h1>
                     <span className={letterClass}>H</span>
                     <span className={`${letterClass} _11`}>i,</span>
-                    <br /> 
+                    <span className={`${letterClass} _11`}> </span>
                     <span className={`${letterClass} _12`}>I</span>
                     <span className={`${letterClass} _13`}>'m</span>
+                    <br/>
 
                 <img src={LogoTitle} alt="developer" />
-                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={14} />            
+                <AnimatedLetters letterClass={letterClass} strArray={nameArray} idx={14} />
                 <br />
-                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={28} />            
+                <AnimatedLetters letterClass={letterClass} strArray={jobArray} idx={28} />
                 </h1>
                 <h2>Mechanical Engineer / Machine Learning Engineer / Data Engineer</h2>
                 <Link to="/contact" className="flat-button">CONTACT ME</Link>
